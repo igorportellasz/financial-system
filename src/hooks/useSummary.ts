d357b0d7fac23ca1171f79ee76c25c0ba5c85666
Igor Portella <igorportella74@gmail.com>
2025-03-21 17:00:01 -0300

    perf: Adicionado useMemo no useSummary para não carregar o summary sem necessidade

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,30 +1,35 @@
+import { useMemo } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
 
 // Hook que calcula o resumo das transações (entrada, saída e total)
 export function useSummary() {
     // Obtém as transações do contexto
-    const transactions = useContextSelector(TransactionsContext, (context) => context.transactions);
+    const transactions = useContextSelector(TransactionsContext, (context) => {
+        return context.transactions
+    });
 
     // Calcula o resumo das transações
-    const summary = transactions.reduce(
-        (accumulator, transaction) => {
-            if (transaction.type === 'income') {
-                accumulator.income += transaction.price; // Soma das entradas
-                accumulator.total += transaction.price; // Total com entradas
-            } else {
-                accumulator.outcome += transaction.price; // Soma das saídas
-                accumulator.total -= transaction.price; // Total com saídas
-            }
+    const summary = useMemo(() => {
+        return transactions.reduce(
+            (accumulator, transaction) => {
+                if (transaction.type === 'income') {
+                    accumulator.income += transaction.price; // Soma das entradas
+                    accumulator.total += transaction.price; // Total com entradas
+                } else {
+                    accumulator.outcome += transaction.price; // Soma das saídas
+                    accumulator.total -= transaction.price; // Total com saídas
+                }
+    
+                return accumulator;
+            }, 
+            { 
+                income: 0, 
+                outcome: 0, 
+                total: 0,
+            },
+        )
+    }, [transactions])
 
-            return accumulator;
-        }, 
-        { 
-            income: 0, 
-            outcome: 0, 
-            total: 0,
-        }
-    );
-
-    return summary; // Retorna o resumo
+    return summary
 }
